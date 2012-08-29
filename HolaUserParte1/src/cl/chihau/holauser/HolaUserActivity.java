3cafc92563ea6d9fd253407b772101cc8b3e4380
Chihau Chau <chihau@gmail.com>
2012-08-29 16:38:40 -0400

    Modificado ejemplo HolaUserParte1

--- HolaUserParte1/src/cl/chihau/holauser/HolaUserActivity.java
+++ HolaUserParte1/src/cl/chihau/holauser/HolaUserActivity.java
@@ -2,28 +2,23 @@ package cl.chihau.holauser;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class HolaUserActivity extends Activity {
+	
+	EditText campoNombre;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.main);
 		
-		/* 
-		 * Relacionamos el bot—n BtnHola perteneciente al main.xml
-		 * con un objeto de tipo Button llamado btnHola
-		 */
-		final Button btnHola = (Button) findViewById(R.id.BtnHola);
-		
 		/*
 		 * Relacionamos el campo de texto TxtNombre perteneciente al 
 		 * main.xml con un objeto de tipo EditText llamado txtNombre
 		 */
-		final EditText txtNombre = (EditText) findViewById(R.id.TxtNombre);
-		
-		
+		campoNombre = (EditText) findViewById(R.id.TxtNombre);
 	}
+	
 }
