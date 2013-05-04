ce9dfe9fb0c064277e3ed273904194e6e7c59a76
Chihau Chau <chihau@gmail.com>
2013-05-04 12:46:06 -0400

    Agregado ejemplo HolaLifeCycle y modificado README.md

---
+++ HolaLifeCycle/src/cl/chihau/holalifecycle/HolaLifeCycleActivity.java
@@ -0,0 +1,56 @@
+package cl.chihau.holalifecycle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HolaLifeCycleActivity extends Activity {
+	static final String TAG = "HolaLifeCycleActivity";
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main);
+		
+		Log.d(TAG, "onCreate");
+	}
+	
+	@Override
+	public void onStart() {
+    	super.onStart();
+		Log.d(TAG, "onStart");
+	}
+	
+	@Override
+	public void onResume() {
+    	super.onResume();
+		Log.d(TAG, "onResume");
+	}
+	
+	@Override
+	public void onPause() {
+    	super.onPause();
+		Log.d(TAG, "onPause");
+	}
+	
+	@Override
+	public void onStop() {
+    	super.onStop();
+		Log.d(TAG, "onStop");
+	}
+	
+	@Override
+	public void onRestart() {
+		super.onRestart();
+		Log.d(TAG, "onRestart");
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy");
+	}
+	
+	
+}
