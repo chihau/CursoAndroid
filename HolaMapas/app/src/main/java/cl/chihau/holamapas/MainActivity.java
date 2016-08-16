e106b0ca137c0c36cc74229836f1291b8d43e6c9
Chihau Chau <chihau@gmail.com>
2016-08-16 17:17:31 -0300

    Actualiza ejemplo HolaMapas

--- HolaMapas/app/src/main/java/cl/chihau/holamapas/MainActivity.java
+++ HolaMapas/app/src/main/java/cl/chihau/holamapas/MainActivity.java
@@ -1,44 +1,49 @@
 package cl.chihau.holamapas;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mapa = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GoogleMap mapa = null;
-
         if (mapa == null) {
-            mapa = ((SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map)).getMap();
+            SupportMapFragment smf = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            smf.getMapAsync(this);
+        }
+    }
 
-            if (mapa != null) {
-                // Agregamos una capa simple de localización (MyLocation Layer)
-                // Para hacer algo más preciso y personalizable utilizar la
-                // Location API
-                // http://developer.android.com/google/play-services/location.html
-                mapa.setMyLocationEnabled(true);
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapa = googleMap;
 
-                mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        if (mapa != null) {
+            mapa.setMyLocationEnabled(true);
 
-                mapa.addMarker(new MarkerOptions()
-                                .position(new LatLng(-33.036652, -71.595088))
-                                .title("Entrada")
-                                .draggable(true)
-                );
-            } else {
-                Log.e("HelloMaps", "No están disponibles los mapas");
-            }
+            mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+            mapa.addMarker(new MarkerOptions()
+                            .position(new LatLng(-33.036652, -71.595088))
+                            .title("Entrada")
+                            .draggable(true)
+            );
+
+        } else {
+            Log.e("HolaMapas", "mapa nulo");
         }
     }
 }
