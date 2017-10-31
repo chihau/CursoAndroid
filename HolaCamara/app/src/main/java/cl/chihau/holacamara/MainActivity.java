61d5e20b4ea738b98250bba65b10920720047f7d
Chihau Chau <chihau@gmail.com>
2017-10-31 17:57:48 -0300

    Actualiza ejemplo HolaCamara, ahora soporta Android 6.0+ ya que ahora se utiliza FileProvider. probado en Android 5.1.1 x86 Nexus 4 (Real) y Android 8.0.0 x86 Nexus 5X (Real)

--- HolaCamara/app/src/main/java/cl/chihau/holacamara/MainActivity.java
+++ HolaCamara/app/src/main/java/cl/chihau/holacamara/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,7 +17,7 @@ import android.widget.Toast;
 
 import java.io.File;
 
-public class HolaCamaraActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     private static final int TAKE_PICTURE = 0;
     Uri mUri;
     Bitmap mPhoto;
@@ -24,7 +25,29 @@ public class HolaCamaraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sacarFoto(View view) {
+        if (view.getId() == R.id.btn_foto &&
+                getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+
+            // Desde Android 5.1 el método recomendado para acceder a los archivos es utilizando
+            // un FileProvider (que es una subclase de ContentProvider)
+            // antiguamente se accedía directamente a través de la uri file:// pero ahora es
+            // a través de content://
+            Intent i = new Intent("android.media.action.IMAGE_CAPTURE");
+            // Antes era File f = new File(Environment.getExternalStorageDirectory(),  "foto.jpg");
+            File f = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),  "foto.jpg");
+
+            // Antes era mUri = Uri.fromFile(f); // Ej: file://sdcard/foto.jpg
+            mUri = FileProvider.getUriForFile(MainActivity.this,
+                    BuildConfig.APPLICATION_ID + ".provider",
+                    f);
+
+            i.putExtra(MediaStore.EXTRA_OUTPUT, mUri);
+            startActivityForResult(i, TAKE_PICTURE);
+        }
     }
 
     @Override
@@ -44,18 +67,5 @@ public class HolaCamaraActivity extends AppCompatActivity {
                     }
                 }
         }
-    }
-
-    public void sacarFoto(View view) {
-        if (view.getId() == R.id.btn_foto &&
-                getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
-
-            Intent i = new Intent("android.media.action.IMAGE_CAPTURE");
-            File f = new File(Environment.getExternalStorageDirectory(),  "foto.jpg");
-            mUri = Uri.fromFile(f); // Ej: /sdcard/foto.jpg
-            i.putExtra(MediaStore.EXTRA_OUTPUT, mUri);
-            startActivityForResult(i, TAKE_PICTURE);
-        }
-
     }
 }
