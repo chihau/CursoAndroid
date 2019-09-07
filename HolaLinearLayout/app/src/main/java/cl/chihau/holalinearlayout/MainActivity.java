d32f6a2a68446dda94c2b5db6bf3e3ac3690f0ba
Chihau Chau <chihau@gmail.com>
2019-09-07 12:36:32 -0400

    Actualiza ejemplo HolaLinearLayout

--- HolaLinearLayout/app/src/main/java/cl/chihau/holalinearlayout/MainActivity.java
+++ HolaLinearLayout/app/src/main/java/cl/chihau/holalinearlayout/MainActivity.java
@@ -3,11 +3,11 @@ package cl.chihau.holalinearlayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HolaLinearLayoutActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
     }
 }
