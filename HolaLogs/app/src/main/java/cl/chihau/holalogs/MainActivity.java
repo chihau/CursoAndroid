bbcc288a62b9c35d05bd5d3dfed1eef8c8d0989a
Chihau Chau <chihau@gmail.com>
2019-09-07 12:35:00 -0400

    Actualiza ejemplo HolaLogs

--- HolaLogs/app/src/main/java/cl/chihau/holalogs/MainActivity.java
+++ HolaLogs/app/src/main/java/cl/chihau/holalogs/MainActivity.java
@@ -4,18 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-/**
- * Created by chihau on 15-10-15.
- */
-public class HolaLogsActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {
     public static final String LOGTAG = "HolaLogsActivity";
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
         Log.e(LOGTAG, "Mensaje de error");
         Log.w(LOGTAG, "Mensaje de advertencia");
