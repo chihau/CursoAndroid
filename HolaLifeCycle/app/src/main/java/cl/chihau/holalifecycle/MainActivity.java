619a584ffd61d2a56940f71e1a31f7e23779cf3e
Chihau Chau <chihau@gmail.com>
2019-09-09 00:18:44 -0300

    Actualiza ejemplo HolaLifeCycle

--- HolaLifeCycle/app/src/main/java/cl/chihau/holalifecycle/MainActivity.java
+++ HolaLifeCycle/app/src/main/java/cl/chihau/holalifecycle/MainActivity.java
@@ -1,58 +1,58 @@
 package cl.chihau.holalifecycle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-public class HolaLifeCycleActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String LOGTAG = "HolaLifeCycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
         Log.d(LOGTAG, "onCreate");
     }
 
     @Override
-    public void onStart() {
+    protected void onStart() {
         super.onStart();
 
         Log.d(LOGTAG, "onStart");
     }
 
     @Override
-    public void onResume() {
+    protected void onResume() {
         super.onResume();
 
         Log.d(LOGTAG, "onResume");
     }
 
     @Override
-    public void onPause() {
+    protected void onPause() {
         super.onPause();
 
         Log.d(LOGTAG, "onPause");
     }
 
     @Override
-    public void onStop() {
+    protected void onStop() {
         super.onStop();
 
         Log.d(LOGTAG, "onStop");
     }
 
     @Override
-    public void onRestart() {
+    protected void onRestart() {
         super.onRestart();
 
         Log.d(LOGTAG, "onRestart");
     }
 
     @Override
-    public void onDestroy() {
+    protected void onDestroy() {
         super.onDestroy();
 
         Log.d(LOGTAG, "onDestroy");
