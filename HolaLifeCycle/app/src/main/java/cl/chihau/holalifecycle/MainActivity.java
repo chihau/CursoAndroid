f5931b10089a6a811a5fc2507482c56d13df9363
Chihau Chau <chihau@gmail.com>
2020-03-29 21:39:45 -0300

    Actualiza ejemplo HolaLifeCycle

--- HolaLifeCycle/app/src/main/java/cl/chihau/holalifecycle/MainActivity.java
+++ HolaLifeCycle/app/src/main/java/cl/chihau/holalifecycle/MainActivity.java
@@ -1,60 +1,53 @@
 package cl.chihau.holalifecycle;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private static final String LOGTAG = "HolaLifeCycle";
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "HolaLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d(LOGTAG, "onCreate");
+        Log.d(TAG, "onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        Log.d(LOGTAG, "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Log.d(LOGTAG, "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Log.d(LOGTAG, "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Log.d(LOGTAG, "onStop");
+        Log.d(TAG, "onStart");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
 
-        Log.d(LOGTAG, "onRestart");
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        Log.d(LOGTAG, "onDestroy");
+        Log.d(TAG, "onDestroy");
     }
 }
